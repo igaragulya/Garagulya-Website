2908bc9a83681e8a09054e0caad89e8c8fe5aa76
Fenny <i.mkrft@gmail.com>
2020-03-31 17:08:51 -0400

    Final PC Com

--- setup.py
+++ setup.py
@@ -9,9 +9,7 @@ def home():
 @app.route('/projects/')
 def projects():
     return render_template("projects.html")
-@app.route('/pricing/')
-def Price():
-    return render_template("pri.html")
+    
 @app.route('/contact/')
 def Con():
     return render_template("Contact.html")
